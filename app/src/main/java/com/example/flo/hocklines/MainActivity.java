2942847fa3490bc753ec0ffb2c17acc780510bbf
fbremec <fbremec@gmail.com>
2017-10-19 18:03:40 +0200

    firebase

--- app/src/main/java/com/example/flo/hocklines/MainActivity.java
+++ app/src/main/java/com/example/flo/hocklines/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.flo.hocklines;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.ThemedSpinnerAdapter;
 import android.util.Log;
@@ -18,16 +22,41 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.model.file_descriptor.FileDescriptorModelLoader;
 import com.example.flo.hocklines.hocklines_timer.fragment.HocklinesTimerFragment;
 import com.example.flo.hocklines.hocklines_timer.events.IncrementWorkEvent;
 import com.example.flo.hocklines.hocklines_timer.events.SleepTimerEvent;
 import com.example.flo.hocklines.hocklines_timer.events.StopSeanceEvent;
 import com.example.flo.hocklines.hocklines_timer.events.WorkTimerEvent;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicMarkableReference;
+
+import static android.R.id.list;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -43,6 +72,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -203,6 +233,8 @@ public class MainActivity extends AppCompatActivity
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
+        private ArrayList<String> nameOfLicences;
+        private ArrayList<File> fileOfLicences = new ArrayList<>();
 
         public PlaceholderFragment() {
         }
@@ -219,12 +251,195 @@ public class MainActivity extends AppCompatActivity
             return fragment;
         }
 
+        private void readNameOfLicences(){
+            nameOfLicences = new ArrayList<>();
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference myRef = database.getReference("licences");
+            myRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    GenericTypeIndicator<List<String>> t = new GenericTypeIndicator<List<String>>(){};
+                    List<String> value = dataSnapshot.getValue(t);
+                    nameOfLicences.addAll(value);
+                    Log.d("LOG",value.get(0));
+                    displayLicences();
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+            Log.d("LOG","END READ");
+        }
+
+        private void dlLicence(String nameOfFile) throws IOException {
+            StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+            StorageReference pathReference = storageRef.child(nameOfFile+".png");
+            final File localFile = File.createTempFile(nameOfFile, ".png");
+
+            pathReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                    // Local temp file has been created
+                    Log.d("LOG","SUCCES :: "+localFile.getAbsolutePath());
+                    fileOfLicences.add(localFile);
+                    Glide.with(getContext()).load(localFile).into(i);
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle any errors
+                    Log.d("LOG","FAIL");
+                }
+            });
+
+        }
+
+        private void displayLicences(){
+            for(String nameOfFile : nameOfLicences){
+                File f = new File(nameOfFile);
+                if(f.exists()) {
+                    Log.d("LOG","EXIST");
+                    fileOfLicences.add(f);
+                    Glide.with(getContext()).load(f).into(i);
+                }else{
+                    Log.d("LOG","NOT EXIST");
+                    try {
+                        this.dlLicence(nameOfFile);
+                        Log.d("LOG","REPRISE V2");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        ImageView i;
+
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main2, container, false);
             TextView textView = (TextView) rootView.findViewById(R.id.section_label);
             textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+            this.i = rootView.findViewById(R.id.image);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    readNameOfLicences();
+                    Log.d("LOG","REPRISE");
+
+                }
+            }).start();
+
+
+
+//            final Context c = getContext();
+//            final ArrayList<String> list = new ArrayList<>();
+//            FirebaseDatabase database = FirebaseDatabase.getInstance();
+//            final DatabaseReference myRef = database.getReference("licences");
+//            myRef.addValueEventListener(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(DataSnapshot dataSnapshot) {
+//                    // This method is called once with the initial value and again
+//                    // whenever data at this location is updated.
+//                    GenericTypeIndicator<List<String>> t = new GenericTypeIndicator<List<String>>() {};
+//                    List<String> value = dataSnapshot.getValue(t);
+//                    final Context c = getContext();
+//                    Log.d("TAG", "Value is: " + value);
+//                    final ArrayList<String> list = new ArrayList<>();
+//                    for(String file : value) {
+//                        File f = new File(file);
+//                        if(f.exists())
+//                            Glide.with(c).load(f).into(i);
+//                        else{
+//                            try {
+//                                StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+//                                StorageReference pathReference = storageRef.child(file+".png");
+//
+//                                final File localFile = File.createTempFile(file, ".png");
+//                                pathReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+//                                    @Override
+//                                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+//                                        // Local temp file has been created
+//                                        Log.d("IMAGE","SUCCES :: "+localFile.getAbsolutePath());
+//
+//                                        list.add(localFile.getAbsolutePath());
+//                                        myRef.setValue(list);
+//                                        Glide.with(c).load(localFile).into(i);
+//
+//                                    }
+//                                }).addOnFailureListener(new OnFailureListener() {
+//                                    @Override
+//                                    public void onFailure(@NonNull Exception exception) {
+//                                        // Handle any errors
+//                                        Log.d("IMAGE","FAIL");
+//                                    }
+//                                });
+//
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+//
+//                    }
+//
+//                }
+//
+//                @Override
+//                public void onCancelled(DatabaseError error) {
+//                    // Failed to read value
+//                    Log.w("TAG", "Failed to read value.", error.toException());
+//                }
+//            });
+
+//            /*File f = new File("/data/user/0/com.example.flo.hocklines/cache/test156392918.png");
+//            if(f.exists())
+//                Glide.with(this).load(f).into(i);
+//            else {*/
+//                Log.d("TAG","NOT EXIST");
+//                StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+//                StorageReference pathReference = storageRef.child("licences/");
+//
+//
+////            Glide.with(this /* context */)
+////                    .using(new FirebaseImageLoader())
+////                    .load(pathReference)
+////                    .into(i);
+//                try {
+//                    final Context c = getContext();
+//                    final File localFile = File.createTempFile("test", ".png");
+//                    pathReference.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+//                        @Override
+//                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+//                            // Local temp file has been created
+//                            Log.d("IMAGE","SUCCES :: "+localFile.getAbsolutePath());
+//                            Glide.with(c).load(localFile).into(i);
+//
+//                        }
+//                    }).addOnFailureListener(new OnFailureListener() {
+//                        @Override
+//                        public void onFailure(@NonNull Exception exception) {
+//                            // Handle any errors
+//                            Log.d("IMAGE","FAIL");
+//                        }
+//                    });
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            //}
+
+
+
+
+
+
             return rootView;
         }
     }
